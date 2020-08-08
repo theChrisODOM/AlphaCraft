94bc8504478ca93e6d90ff413cd86ef7cc99a94d
theChrisODOM <csodom22@gmail.com>
2020-08-07 20:45:00 -0500

    added nasty stem block

---
+++ src/main/java/com/bellatorex/alphacraft/blocks/DarkStemBlock.java
@@ -0,0 +1,16 @@
+package com.bellatorex.alphacraft.blocks;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.common.ToolType;
+
+public class DarkStemBlock extends Block {
+    public DarkStemBlock() {
+        super(Block.Properties.create(Material.WOOD)
+                .hardnessAndResistance(1.5f,3.0f)
+                .sound(SoundType.NETHER_SPROUT)
+                .harvestLevel(1)
+                .harvestTool(ToolType.AXE));
+    }
+}
--- src/main/java/com/bellatorex/alphacraft/util/BlockRegistry.java
+++ src/main/java/com/bellatorex/alphacraft/util/BlockRegistry.java
@@ -3,6 +3,7 @@ package com.bellatorex.alphacraft.util;
 import com.bellatorex.alphacraft.AlphaCraft;
 import com.bellatorex.alphacraft.blocks.*;
 import net.minecraft.block.Block;
+import net.minecraft.block.Blocks;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -24,15 +25,17 @@ public class BlockRegistry {
 
     public static final RegistryObject<Block> DARK_DIRT_BLOCK = BLOCKS.register("dark_dirt_block", DarkEarthBlock::new);
     public static final RegistryObject<Block> DARK_GRASS_BLOCK = BLOCKS.register("dark_grass_block", DarkGrassBlock::new);
-    public static final RegistryObject<Block> CASTLE_BLOCK = BLOCKS.register("castle_block", CastleBlock::new);
-    public static final RegistryObject<Block> ENDERITE_BLOCK = BLOCKS.register("enderite_block", EnderiteBlock::new);
-    public static final RegistryObject<Block> ENDERITE_ORE_BLOCK = BLOCKS.register("enderite_ore_block", EnderiteOreBlock::new);
+    public static final RegistryObject<Block> NASTY_STEM = BLOCKS.register("nasty_stem", DarkStemBlock::new);
     public static final RegistryObject<Block> DARK_CLAY_BLOCK = BLOCKS.register("dark_clay_block", DarkEarthBlock::new);
     public static final RegistryObject<Block> POOP_BLOCK = BLOCKS.register("poop_block", PoopBlock::new);
+    public static final RegistryObject<Block> ENDERITE_ORE_BLOCK = BLOCKS.register("enderite_ore_block", EnderiteOreBlock::new);
+    public static final RegistryObject<Block> CASTLE_BLOCK = BLOCKS.register("castle_block", CastleBlock::new);
+    public static final RegistryObject<Block> ENDERITE_BLOCK = BLOCKS.register("enderite_block", EnderiteBlock::new);
 
     //Block Items
     public static final RegistryObject<Item> DARK_DIRT_BLOCK_ITEM = ITEMS.register("dark_dirt_block", () -> new BlockItemBase(DARK_DIRT_BLOCK.get()));
     public static final RegistryObject<Item> DARK_GRASS_BLOCK_ITEM = ITEMS.register("dark_grass_block", () -> new BlockItemBase(DARK_GRASS_BLOCK.get()));
+    public static final RegistryObject<Item> NASTY_STEM_ITEM = ITEMS.register("nasty_stem", () -> new BlockItemBase(NASTY_STEM.get()));
     public static final RegistryObject<Item> CASTLE_BLOCK_ITEM = ITEMS.register("castle_block", () -> new BlockItemBase(CASTLE_BLOCK.get()));
     public static final RegistryObject<Item> ENDERITE_BLOCK_ITEM = ITEMS.register("enderite_block", () -> new BlockItemBase(ENDERITE_BLOCK.get()));
     public static final RegistryObject<Item> ENDERITE_ORE_BLOCK_ITEM = ITEMS.register("enderite_ore_block", () -> new BlockItemBase(ENDERITE_ORE_BLOCK.get()));
